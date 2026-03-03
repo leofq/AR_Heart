0ec4ac2638e8b36bc156f848fb62b7de0513459f
leofq <2203432@uad.ac.uk>
2026-03-03 15:39:45 +0000

    Deleting un-used filed and tidying comments

--- Assets/_ARMarker/Scripts/LabelledObject.cs
+++ Assets/_ARMarker/Scripts/LabelledObject.cs
@@ -15,6 +15,7 @@ public class LabelledObject : MonoBehaviour
     [SerializeField] TMP_Text m_text;
     public Transform label;
 
+    // width of line renderer
     [SerializeField] float linewidth = 0.001f;
 
     Vector2 mainCam;
@@ -28,14 +29,13 @@ public class LabelledObject : MonoBehaviour
         m_text = GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         // make label face camera
         label.transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
         label.Rotate(0, 180f, 0);
 
-        // Draw the line between source and target
+        // Draw the line between source and target using line renderer
         if (source != null && target != null)
         {
             Vector3[] pos = new Vector3[] { source.transform.position, target.transform.position };
